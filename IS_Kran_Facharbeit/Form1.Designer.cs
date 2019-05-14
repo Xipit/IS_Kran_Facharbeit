211a1dcaae8203126ee5011c5248b6cf25599dbb
Hanno Witzleb <hanno2604@gmx.de>
2019-05-14 17:08:30 +0200

    1234

--- IS_Kran_Facharbeit/Form1.Designer.cs
+++ IS_Kran_Facharbeit/Form1.Designer.cs
@@ -110,7 +110,7 @@
             this.InfoBtn.TabIndex = 2;
             this.InfoBtn.Text = "Info";
             this.InfoBtn.UseVisualStyleBackColor = true;
-            this.InfoBtn.Click += new System.EventHandler(this.InfoBtn_Click);
+           // this.InfoBtn.Click += new System.EventHandler(this.InfoBtn_Click);
             // 
             // ZuruecksetzenBtn
             // 
@@ -120,7 +120,7 @@
             this.ZuruecksetzenBtn.TabIndex = 1;
             this.ZuruecksetzenBtn.Text = "Zurücksetzen";
             this.ZuruecksetzenBtn.UseVisualStyleBackColor = true;
-            this.ZuruecksetzenBtn.Click += new System.EventHandler(this.ZuruecksetzenBtn_Click);
+           // this.ZuruecksetzenBtn.Click += new System.EventHandler(this.ZuruecksetzenBtn_Click);
             // 
             // SchliessenBtn
             // 
